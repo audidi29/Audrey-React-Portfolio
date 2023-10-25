c8574ac09b2bc512b856a3d00df204c4f5337a32
Your Name <you@example.com>
2023-10-25 13:53:22 -0700

    adding property to vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 8003,
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 8003,
   },
-})
+  build: {
+    outDir: 'build', // Output directory
+  },
+
+base: './',
+
+});
